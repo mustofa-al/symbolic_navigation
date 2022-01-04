d5c75940cb5fc700d5aad9b6b2e41fb4a1aaaabe
Ali Mustofa <mustofa@extrainteger.com>
2022-01-04 16:31:34 +0700

    add selection transition animation

--- lib/sym_desktop_nav.dart
+++ lib/sym_desktop_nav.dart
@@ -2,6 +2,7 @@ library sym_desktop_nav;
 
 import 'package:flutter/material.dart';
 import 'package:sym_desktop_nav/sym_desktop_nav/sym_desktop_nav_item.dart';
+import 'package:sym_desktop_nav/sym_desktop_nav/sym_desktop_nav_selection.dart';
 
 class SymDesktopNav extends StatefulWidget {
   final List<SymDesktopNavItem> items;
@@ -10,16 +11,20 @@ class SymDesktopNav extends StatefulWidget {
   final Color? backgroundColor;
   final Color? itemBackgroundColor;
   final TextStyle? textStyle;
+  final Curve curve;
+  final Duration duration;
 
-  const SymDesktopNav({
-    Key? key,
-    required this.items,
-    required this.initialIndex,
-    required this.onItemSelected,
-    this.backgroundColor,
-    this.textStyle,
-    this.itemBackgroundColor,
-  }) : super(key: key);
+  const SymDesktopNav(
+      {Key? key,
+      required this.items,
+      required this.initialIndex,
+      required this.onItemSelected,
+      this.backgroundColor,
+      this.textStyle,
+      this.itemBackgroundColor,
+      this.curve = Curves.fastLinearToSlowEaseIn,
+      this.duration = const Duration(milliseconds: 500)})
+      : super(key: key);
 
   @override
   _SymDesktopNavState createState() => _SymDesktopNavState();
@@ -37,31 +42,61 @@ class _SymDesktopNavState extends State<SymDesktopNav> {
     return Material(
       color: widget.backgroundColor ?? const Color(0xFF5e4229),
       textStyle: widget.textStyle,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: categorizedItems
-            .asMap()
-            .map((key, value) => MapEntry(
-                key,
-                Column(
-                  children: value
-                      .asMap()
-                      .map(
-                          (key1, value1) => MapEntry(key1, _item(key1, value1)))
-                      .values
-                      .toList(),
-                )))
-            .values
-            .toList(),
+      child: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 16,
+              bottom: 16,
+            ),
+            child: SymDesktopNavSelection(
+              height: 44,
+              width: 44,
+              offsetY: _getYOffset(),
+              color: const Color(0xFFB26941).withOpacity(0.5),
+              duration: widget.duration,
+              curve: widget.curve,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: categorizedItems
+                .asMap()
+                .map(
+                  (groupIndex, groupValue) => MapEntry(
+                    groupIndex,
+                    Column(
+                      children: groupValue
+                          .asMap()
+                          .map(
+                            (itemIndex, itemValue) => MapEntry(
+                              itemIndex,
+                              _item(groupIndex, itemIndex, groupValue.length,
+                                  itemValue),
+                            ),
+                          )
+                          .values
+                          .toList(),
+                    ),
+                  ),
+                )
+                .values
+                .toList(),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _item(int key, SymDesktopNavItem value) {
+  Widget _item(int groupIndex, int valueIndex, int groupValuesLength,
+      SymDesktopNavItem value) {
     return Padding(
       padding: EdgeInsets.only(
-        top: key == 0 ? 16 : 8,
-        bottom: key == widget.items.length ? 16 : 8,
+        top: (value.classify == Classify.top && valueIndex == 0) ? 16 : 8,
+        bottom: (value.classify == Classify.bottom &&
+                valueIndex == groupValuesLength - 1)
+            ? 16
+            : 8,
         left: 8,
         right: 8,
       ),
@@ -74,15 +109,24 @@ class _SymDesktopNavState extends State<SymDesktopNav> {
             const Color(0xFFFFFFFF).withOpacity(0.1),
         child: Ink(
           padding: const EdgeInsets.all(6),
-          decoration: value == widget.items[widget.initialIndex]
-              ? BoxDecoration(
-                  color: const Color(0xFFB26941).withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(8),
-                )
-              : null,
+          // decoration: value == widget.items[widget.initialIndex]
+          //     ? BoxDecoration(
+          //         color: const Color(0xFFB26941).withOpacity(0.5),
+          //         borderRadius: BorderRadius.circular(8),
+          //       )
+          //     : null,
           child: value,
         ),
       ),
     );
+  }
+
+  double _getYOffset() {
+    double offsetY = (16.0 + 44.0) * widget.initialIndex;
+    if (widget.items[widget.initialIndex].classify == Classify.bottom) {
+      offsetY = ((MediaQuery.of(context).size.height - 16.0) -
+          (44.0 + 16.0) * (widget.items.length - widget.initialIndex));
+    }
+    return offsetY;
   }
 }
