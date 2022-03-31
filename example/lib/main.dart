8bc2e5f6a95378b1375aa97ed0ea9d2a6ceeff5c
Ali Mustofa <mustofa@extrainteger.com>
2022-03-31 19:06:33 +0700

    code now in single file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sym_nav/sym_desktop_nav.dart';
-import 'package:sym_nav/sym_desktop_nav/sym_desktop_nav_item.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/sym_desktop_nav.dart
+++ lib/sym_desktop_nav.dart
@@ -1,8 +1,157 @@
 library sym_desktop_nav;
 
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
-import 'package:sym_nav/sym_desktop_nav/sym_desktop_nav_item.dart';
-import 'package:sym_nav/sym_desktop_nav/sym_desktop_nav_selection.dart';
+
+abstract class SymDesktopNavItem extends StatelessWidget {
+  final String itemName;
+  final Widget? itemImage;
+  const SymDesktopNavItem({
+    Key? key,
+    required this.itemName,
+    this.itemImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      message: itemName,
+      waitDuration: const Duration(seconds: 1),
+      child: Ink(
+        height: 32,
+        width: 32,
+        child: _getView(),
+      ),
+    );
+  }
+
+  Widget _getView() {
+    Widget w = Ink(
+      height: 32,
+      width: 32,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+            .withOpacity(1.0),
+      ),
+      child: Center(
+        child: Text(
+          itemName.substring(0, 1).toUpperCase(),
+        ),
+      ),
+    );
+    if (itemImage != null) {
+      w = itemImage!;
+    }
+    return w;
+  }
+}
+
+class SymDesktopNavTopItem extends SymDesktopNavItem {
+  final bool withSeparator;
+  const SymDesktopNavTopItem({
+    Key? key,
+    required String itemName,
+    Widget? itemImage,
+    this.withSeparator = false,
+  }) : super(key: key, itemName: itemName, itemImage: itemImage);
+}
+
+class SymDesktopNavBottomItem extends SymDesktopNavItem {
+  const SymDesktopNavBottomItem({
+    Key? key,
+    required String itemName,
+    Widget? itemImage,
+  }) : super(key: key, itemName: itemName, itemImage: itemImage);
+}
+
+class SymDesktopNavSelection extends StatefulWidget {
+  final double height, width, offsetY;
+  final Color color;
+  final Duration duration;
+  final Curve curve;
+  const SymDesktopNavSelection({
+    Key? key,
+    required this.height,
+    required this.width,
+    required this.offsetY,
+    required this.color,
+    required this.duration,
+    required this.curve,
+  }) : super(key: key);
+
+  @override
+  _SymDesktopNavSelectionState createState() => _SymDesktopNavSelectionState();
+}
+
+class _SymDesktopNavSelectionState extends State<SymDesktopNavSelection>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _offsetYAnimation;
+  late CurvedAnimation _curvedAnimation;
+  late double _oldOffsetY;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    );
+
+    _curvedAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: widget.curve,
+    );
+
+    _offsetYAnimation = _createAnimation(0, widget.offsetY);
+
+    _oldOffsetY = widget.offsetY;
+    _controller.addListener(() => setState(() {}));
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(SymDesktopNavSelection oldWidget) {
+    if (_oldOffsetY == widget.offsetY) return;
+
+    _offsetYAnimation = _createAnimation(_oldOffsetY, widget.offsetY);
+    _oldOffsetY = widget.offsetY;
+    _controller.reset();
+    _controller.forward();
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  Animation<double> _createAnimation(double begin, double end) {
+    return Tween<double>(
+      begin: begin,
+      end: end,
+    ).animate(_curvedAnimation);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(8, _offsetYAnimation.value),
+      child: Container(
+        width: widget.width,
+        height: widget.height,
+        decoration: BoxDecoration(
+          color: widget.color,
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
 
 class SymDesktopNav extends StatefulWidget {
   final List<SymDesktopNavItem> items;
